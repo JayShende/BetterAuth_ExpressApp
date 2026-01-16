e91e0998b7cefbb8a61c6d1a413791c90a33cf65
JayShende <shendejay379@gmail.com>
2026-01-16 18:28:17 +0530

    init app express BetterAuth

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
+
+
+const app = express();
+const port = 3001;
+
+// app.all("/api/auth/*", toNodeHandler(auth)); // For ExpressJS v4
+app.all("/api/auth/*splat", toNodeHandler(auth)); // For ExpressJS v5 
+
+// Mount express json middleware after Better Auth handler
+// or only apply it to routes that don't interact with Better Auth
+app.use(express.json());
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
